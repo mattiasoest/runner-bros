e7825e2c55ead088b8c064525666af843b88059d
Mattias <osth.mattias@gmail.com>
2018-03-31 20:33:12 +0900

    Added assets for the maps.

---
+++ android/assets/maps/tiles_2.tsx
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles_2" tilewidth="32" tileheight="32" tilecount="16" columns="16">
+ <image source="../../../../MAPS_TILES/MAPS_runner bros/wintertiles/tiles_2.png" width="512" height="32"/>
+ <tile id="3">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+</tileset>
