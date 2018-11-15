aadf669af3b1b6be4a9897c359f1e566b837545d
Mattias <osth.mattias@gmail.com>
2018-11-15 20:03:34 +0900

    Deleted unsued classes from the design phase.

--- core/src/net/runnerbros/entities/HealthItem.java
+++
@@ -1,8 +0,0 @@
-package net.runnerbros.entities;
-
-public class HealthItem extends BoxItem {
-	
-	public HealthItem(float x, float y, float width, float height, float maxPos) {
-		super(x, y, width, height, maxPos);
-	}
-}
